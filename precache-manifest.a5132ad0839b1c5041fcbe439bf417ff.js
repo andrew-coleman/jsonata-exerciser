d5f9fb703dfaf1fa44b6c16d4d05007ff9e2a637
andrew-coleman <andrew_coleman@uk.ibm.com>
2020-01-24 20:59:43 +0000

    Updates

---
+++ precache-manifest.a5132ad0839b1c5041fcbe439bf417ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9437fc25fd029b456cdd",
+    "url": "/jsonata-exerciser/static/js/runtime~main.9437fc25.js"
+  },
+  {
+    "revision": "3eb3a3533e61336a2b33",
+    "url": "/jsonata-exerciser/static/css/main.203bc65c.chunk.css"
+  },
+  {
+    "revision": "5a5420e1f294ae1686759f891f985708",
+    "url": "/jsonata-exerciser/static/media/Slack_Mark_Monochrome_White.5a5420e1.svg"
+  },
+  {
+    "revision": "86eb5d733eaa007f65f0",
+    "url": "/jsonata-exerciser/static/js/1.86eb5d73.chunk.js"
+  },
+  {
+    "revision": "b1cf98a160006b0c5420",
+    "url": "/jsonata-exerciser/static/js/2.b1cf98a1.chunk.js"
+  },
+  {
+    "revision": "3eb3a3533e61336a2b33",
+    "url": "/jsonata-exerciser/static/js/main.3eb3a353.chunk.js"
+  },
+  {
+    "revision": "390d4488b6c2ee3380b9182751248475",
+    "url": "/jsonata-exerciser/static/media/share.390d4488.svg"
+  },
+  {
+    "revision": "86eb5d733eaa007f65f0",
+    "url": "/jsonata-exerciser/static/css/1.e3f8efee.chunk.css"
+  },
+  {
+    "revision": "cc973e87051c1aad26e3271658b33b48",
+    "url": "/jsonata-exerciser/json.worker.js"
+  },
+  {
+    "revision": "5c1ccb72db6ec976028ed8a60a303ac3",
+    "url": "/jsonata-exerciser/index.html"
+  },
+  {
+    "revision": "3aedea04ccd2005499f587a383cea1b9",
+    "url": "/jsonata-exerciser/editor.worker.js"
+  }
+];
